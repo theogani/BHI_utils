c7a5d1ef691c381751f013fc87c72529e9d5d276
theogani <theogani94@gmail.com>
2025-06-03 13:05:50 +0300

    Initial commit.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='BHI_utils',
-   version='3.1',
+   version='4.0',
    description='Utilities for BHI 2025',
    author='Theofanis Ganitidis',
    packages=['BHI_utils'],
